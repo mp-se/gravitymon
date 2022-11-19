1c3784a22384fae67fc1ffc2f173767caa39c1c0
Magnus <mp-se@users.noreply.github.com>
2022-11-19 10:00:36 +0100

    Added serial pins for esp8266

--- src/helper.cpp
+++ src/helper.cpp
@@ -238,9 +238,8 @@ void printBuildOptions() {
 SerialDebug::SerialDebug(const uint32_t serialSpeed) {
   // Start serial with auto-detected rate (default to defined BAUD)
 #if defined(USE_SERIAL_PINS) && defined(ESP8266)
-  // EspSerial.begin(serialSpeed, SERIAL_8N1, 3, 1);
-  EspSerial.begin(serialSpeed);
-#warning "SerialPins is not implemented on ESP8266"
+  uint8_t txPin = 3;
+  EspSerial.begin(serialSpeed, SERIAL_8N1, SERIAL_TX_ONLY, txPin);
 #elif defined(ESP8266)
   EspSerial.begin(serialSpeed);
 #elif defined(USE_SERIAL_PINS) && defined(ESP32C3)
