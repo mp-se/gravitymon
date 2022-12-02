a191f6bb35009a87f5ff234c3ac72d869ed6d3ef
Magnus <mp-se@users.noreply.github.com>
2022-12-02 10:07:06 +0100

    Added config unit tests

--- src/tests/tests_config.cpp
+++ src/tests/tests_config.cpp
@@ -21,20 +21,52 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
-#include <Arduino.h>
-#include <main.hpp>
+#include <config.hpp>
 #include <AUnit.h>
 
 test(config_defaultValues) {
-  Serial.println("Not implemented yet (config_defaultValues)");
+  assertEqual( myConfig.getGravityFormat(), 'G' );
+  assertEqual( myConfig.getTempFormat(), 'C' );
+  assertEqual( myConfig.getSleepInterval(),  900);
+  assertEqual( myConfig.getTempSensorAdjC(), 0.0);
+  float f = 4.15; 
+  assertEqual( myConfig.getVoltageConfig(), f);
+}
+
+test(config_advDefaultValues) {
+  assertEqual( myAdvancedConfig.getDefaultCalibrationTemp(), 20.0 );
+  assertEqual( myAdvancedConfig.getGyroReadCount(), 50 );
+  assertEqual( myAdvancedConfig.getGyroReadDelay(), 3150 );
+  assertEqual( myAdvancedConfig.getGyroSensorMovingThreashold(), 500 );
+  assertEqual( myAdvancedConfig.getMaxFormulaCreationDeviation(), 3.0 );
+  assertEqual( myAdvancedConfig.getPushIntervalHttp1(), 0 );
+  assertEqual( myAdvancedConfig.getPushIntervalHttp2(), 0 );
+  assertEqual( myAdvancedConfig.getPushIntervalHttp3(), 0 );
+  assertEqual( myAdvancedConfig.getPushIntervalMqtt(), 0 );
+  assertEqual( myAdvancedConfig.getPushIntervalInflux(), 0 );
+  assertEqual( myAdvancedConfig.getPushTimeout(), 10 );
+  assertEqual( myAdvancedConfig.getTempSensorResolution(), 9 );
+  assertEqual( myAdvancedConfig.getWifiConnectTimeout(), 20 );
+  assertEqual( myAdvancedConfig.getWifiPortalTimeout(), 120);
+  assertEqual( myAdvancedConfig.isIgnoreLowAnges(), false);
 }
 
 test(config_tempFormat) {
-  Serial.println("Not implemented yet (config_tempFormat)");
+  myConfig.setTempFormat('F');
+  assertEqual( myConfig.getTempFormat(), 'F' );
+  myConfig.setTempFormat('C');
+  assertEqual( myConfig.getTempFormat(), 'C' );
+  myConfig.setTempFormat('X');
+  assertEqual( myConfig.getTempFormat(), 'C' );
 }
 
 test(config_gravityFormat) {
-  Serial.println("Not implemented yet (config_gravityFormat)");
+  myConfig.setGravityFormat('P');
+  assertEqual( myConfig.getGravityFormat(), 'P' );
+  myConfig.setGravityFormat('G');
+  assertEqual( myConfig.getGravityFormat(), 'G' );
+  myConfig.setGravityFormat('X');
+  assertEqual( myConfig.getGravityFormat(), 'G' );
 }
 
 // EOF
