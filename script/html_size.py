570d4290bf8fc161e266800c38ad8b08870e0e60
Magnus <mp-se@users.noreply.github.com>
2024-01-13 16:18:01 +0100

    Remove debug

--- script/html_size.py
+++ script/html_size.py
@@ -5,13 +5,7 @@ Import("env")
 html_files = env.GetProjectOption("board_build.embed_txtfiles").split()
 print( html_files )
 
-#print(env["CPPDEFINES"])
-
 for html in html_files:
-    #print(html)
     tag = "-D" + html.upper().replace("HTML/", "SIZE_").replace(".", "_")
     val = str(os.stat(html).st_size)
-    #print( tag, val)
     env.Append(CPPDEFINES = (tag, val))
-
-#print(env["CPPDEFINES"])
