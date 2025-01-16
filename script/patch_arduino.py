48c208c2f95228b4a554f082a0cad24501094f1e
simon <simon.idelhauser@gmx.de>
2025-01-16 22:55:29 +0100

    add patch, since we need a valid LED-Config

--- script/patch_arduino.py
+++ script/patch_arduino.py
@@ -7,6 +7,6 @@ dir = env.GetLaunchDir()
 
 if name == "gravity32c3-olimex-release" :
     source = dir + "/patch/olimex/pins_arduino.h"
-    target = framework_dir + "/variants/esp_c3_m1_i_kit/pins_arduino.h"
-#    print( "Copy file : " + source + " -> " + target )
-#    shutil.copyfile( source, target )
+    target = framework_dir + "/variants/esp32c3/pins_arduino.h"
+    print( "Copy file : " + source + " -> " + target )
+    shutil.copyfile( source, target )
