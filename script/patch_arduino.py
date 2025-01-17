c7f8825b24c6018106a9db3e6664c2b9c010a167
simon <simon.idelhauser@gmx.de>
2025-01-17 21:12:33 +0100

    use standard pio for patching

--- script/patch_arduino.py
+++ script/patch_arduino.py
@@ -1,12 +1,14 @@
 Import("env")
 import shutil
+import pathlib
 
 framework_dir = env.PioPlatform().get_package_dir("framework-arduinoespressif32")
 name = env.get( "PIOENV" )#print(FRAMEWORK_DIR)
+board = env.BoardConfig().get_brief_data()['id']
 dir = env.GetLaunchDir()
 
-if name == "gravity32c3-olimex-release" :
-    source = dir + "/patch/olimex/pins_arduino.h"
-    target = framework_dir + "/variants/esp32c3/pins_arduino.h"
-    print( "Copy file : " + source + " -> " + target )
-    shutil.copyfile( source, target )
+source = dir + "/boards/"+ board +".pins.h"
+pathlib.Path(framework_dir + "/variants/"+ board).mkdir(parents=True, exist_ok=True)
+target = framework_dir + "/variants/"+ board +"/pins_arduino.h"
+print( "Copy file : " + source + " -> " + target )
+shutil.copyfile( source, target )
