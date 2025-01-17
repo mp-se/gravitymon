62fac808ba5d2605b99dbafcc0f5945484f87658
simon <simon.idelhauser@gmx.de>
2025-01-17 21:32:38 +0100

    set variant dir

--- script/flash.py
+++ script/flash.py
@@ -5,8 +5,8 @@ if args != 3:
   print("Usage: flash.py <target> <port>")
   exit(0)
 
-if sys.argv[1] != "esp8266" and sys.argv[1] != "esp32" and sys.argv[1] != "esp32s2" and sys.argv[1] != "esp32s3" and sys.argv[1] != "esp32c3-olimex" and sys.argv[1] != "esp32c3" and sys.argv[1] != "esp32lite":
-  print("Supported targets are: esp8266, esp32, esp32s2, esp32s3, esp32c3, esp32c3-olimex or esp32lite")
+if sys.argv[1] != "esp8266" and sys.argv[1] != "esp32" and sys.argv[1] != "esp32s2" and sys.argv[1] != "esp32s3" and sys.argv[1] != "esp32c3" and sys.argv[1] != "esp32lite":
+  print("Supported targets are: esp8266, esp32, esp32s2, esp32s3, esp32c3 or esp32lite")
   exit(0)
 
 flash = ""
@@ -20,8 +20,6 @@ elif sys.argv[1] == "esp32s2":
   flash = " --chip esp32s2 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x1000 bin/bootloader32s2.bin 0x8000 bin/partitions32s2.bin 0xe000 bin/boot32s2.bin 0x10000 bin/firmware32s2.bin"
 elif sys.argv[1] == "esp32s3":
   flash = " --chip esp32s3 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x0000 bin/bootloader32s3.bin 0x8000 bin/partitions32s3.bin 0xe000 bin/boot32s3.bin 0x10000 bin/firmware32s3.bin"
-elif sys.argv[1] == "esp32c3-olimex":
-  flash = " --chip esp32c3 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x0000 bin/bootloader32c3.bin 0x8000 bin/partitions32c3-olimex.bin 0xe000 bin/boot32c3.bin 0x10000 bin/firmware32c3-olimex.bin"
 elif sys.argv[1] == "esp32c3":
   flash = " --chip esp32c3 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x0000 bin/bootloader32c3.bin 0x8000 bin/partitions32c3.bin 0xe000 bin/boot32c3.bin 0x10000 bin/firmware32c3.bin"
 elif sys.argv[1] == "esp32lite":
