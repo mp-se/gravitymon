554589f807791b2fc238c2969e88735e3fa8d280
Magnus <mp-se@noreply.github.com>
2025-01-18 12:09:33 +0100

    Added exception handling for patch script so it works in esp8266 target

--- script/patch_platformio.py
+++ script/patch_platformio.py
@@ -1,18 +1,21 @@
 Import("env")
 import shutil
 
-framework_dir = env.PioPlatform().get_package_dir("framework-arduinoespressif32")
-dir = env.GetLaunchDir()
+try:
+    framework_dir = env.PioPlatform().get_package_dir("framework-arduinoespressif32")
+    dir = env.GetLaunchDir()
 
-platform_dir = env.PioPlatform().get_dir()
+    platform_dir = env.PioPlatform().get_dir()
 
-source = dir+ "/patch/platforms/espressif32/"
-target = platform_dir + "/"
-print( "Copy directory : " + source + " -> " + target )
-shutil.copytree(source, target, dirs_exist_ok=True)
+    source = dir+ "/patch/platforms/espressif32/"
+    target = platform_dir + "/"
+    print( "Copy directory : " + source + " -> " + target )
+    shutil.copytree(source, target, dirs_exist_ok=True)
 
-source = dir + "/patch/packages/framework-arduinoespressif32/"
-target = framework_dir + "/"
-print( "Copy directory : " + source + " -> " + target )
-shutil.copytree(source, target, dirs_exist_ok=True)
+    source = dir + "/patch/packages/framework-arduinoespressif32/"
+    target = framework_dir + "/"
+    print( "Copy directory : " + source + " -> " + target )
+    shutil.copytree(source, target, dirs_exist_ok=True)
+except:
+    pass
 
